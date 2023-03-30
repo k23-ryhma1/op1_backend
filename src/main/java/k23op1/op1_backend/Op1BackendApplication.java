0d41a0fcbd6da430490f5e5ddbfb3c9b7e790f6f
elinarou <elina_rouvinen@outlook.com>
2023-03-30 10:30:23 +0300

    Spring initialize

---
+++ src/main/java/k23op1/op1_backend/Op1BackendApplication.java
@@ -0,0 +1,13 @@
+package k23op1.op1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Op1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Op1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/k23op1/op1_backend/Op1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package k23op1.op1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Op1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
