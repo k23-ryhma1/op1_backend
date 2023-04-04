39512e39c4e2df2cc70523dd6760f2ee9b584884
SProkopios <s.koponen93@gmail.com>
2023-04-04 09:19:35 +0300

    lisätty delete toiminnallisuus

--- src/main/java/k23op1/op1_backend/web/ClothingController.java
+++ src/main/java/k23op1/op1_backend/web/ClothingController.java
@@ -3,7 +3,9 @@ package k23op1.op1_backend.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import k23op1.op1_backend.domain.ClothingRepository;
 import k23op1.op1_backend.domain.ManufacturerRepository;
@@ -18,6 +20,11 @@ public class ClothingController {
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("list", clothingRepository.findAll());
-        return "/";
+        return "index";
     } 
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String delete(@PathVariable("id") Long clothingId, Model model) {
+        clothingRepository.deleteById(clothingId);
+        return ("redirect:../index");
+    }
 }
