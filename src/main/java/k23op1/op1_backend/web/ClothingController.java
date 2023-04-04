2387f968667d10430576acbc599eef12d3933bfe
SProkopios <s.koponen93@gmail.com>
2023-04-04 08:57:00 +0300

    Lisätty findAll

--- src/main/java/k23op1/op1_backend/web/ClothingController.java
+++ src/main/java/k23op1/op1_backend/web/ClothingController.java
@@ -2,6 +2,8 @@ package k23op1.op1_backend.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import k23op1.op1_backend.domain.ClothingRepository;
 import k23op1.op1_backend.domain.ManufacturerRepository;
@@ -13,4 +15,9 @@ public class ClothingController {
     @Autowired
     private ManufacturerRepository manufacturerRepository;
 
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("list", clothingRepository.findAll());
+        return "/";
+    } 
 }
