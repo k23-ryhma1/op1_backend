50641f2dde07cd5ab274fc3b0b8779c7fd734b2f
elinarou <elina_rouvinen@outlook.com>
2023-04-09 19:28:42 +0300

    ManufacturerRestController

---
+++ src/main/java/k23op1/op1_backend/web/ManufacturerRestController.java
@@ -0,0 +1,31 @@
+package k23op1.op1_backend.web;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import k23op1.op1_backend.domain.Manufacturer;
+import k23op1.op1_backend.domain.ManufacturerRepository;
+
+@RestController
+public class ManufacturerRestController {
+
+    @Autowired
+    ManufacturerRepository manufacturerRepository;
+
+    // Palauttaa kaikki valmistajat
+    @GetMapping("/manufacturers")
+    public Iterable<Manufacturer> getManufacturers() {
+        return manufacturerRepository.findAll();
+    }
+    
+    // Palauttaa yhden valmistajan
+    @GetMapping("/manufacturers/{id}")
+    Optional<Manufacturer> getClothing(@PathVariable Long id) {
+        return manufacturerRepository.findById(id);
+    }
+    
+}
