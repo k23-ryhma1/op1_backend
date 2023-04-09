bee7de7689b627db1fe35541ee4d9cd17b013666
elinarou <elina_rouvinen@outlook.com>
2023-04-09 18:15:24 +0300

    ClothingRestController

---
+++ src/main/java/k23op1/op1_backend/web/ClothingRestController.java
@@ -0,0 +1,31 @@
+package k23op1.op1_backend.web;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import k23op1.op1_backend.domain.Clothing;
+import k23op1.op1_backend.domain.ClothingRepository;
+
+@RestController
+public class ClothingRestController {
+
+    @Autowired
+    ClothingRepository clothingRepository;
+
+    // Palauttaa kaikki vaatteet
+    @GetMapping("/clothings")
+    public Iterable<Clothing> getClothings() {
+        return clothingRepository.findAll();
+    }
+    
+    // Palauttaa yhden vaatteen
+    @GetMapping("/clothings/{id}")
+    Optional<Clothing> getClothing(@PathVariable Long id) {
+        return clothingRepository.findById(id);
+    }
+    
+}
