428daec7fdc96e1cc82e113c87249bb23aa60dd8
SProkopios <s.koponen93@gmail.com>
2023-04-13 09:44:23 +0300

    lisätty delete manufacturer

--- src/main/java/k23op1/op1_backend/web/ManufacturerController.java
+++ src/main/java/k23op1/op1_backend/web/ManufacturerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import k23op1.op1_backend.domain.Manufacturer;
@@ -31,5 +32,11 @@ public class ManufacturerController {
     public String listManufacturers(Model model) {
         model.addAttribute("manufacturers", manufacturerRepository.findAll());
         return "listmanufacturers";
+    }
+
+    @GetMapping("/deletemanufacturers/{id}")
+    public String deleteClothing(@PathVariable("id") Long manufacturerId, Model model) {
+        manufacturerRepository.deleteById(manufacturerId);
+        return "redirect:../listmanufacturers";
     }
 }
