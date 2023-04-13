2b2a0de4453560e8977c6874b1bbfb33458fccfe
SProkopios <s.koponen93@gmail.com>
2023-04-13 09:01:00 +0300

    Lisätty manufacturer controlleriin add

--- src/main/java/k23op1/op1_backend/web/ManufacturerController.java
+++ src/main/java/k23op1/op1_backend/web/ManufacturerController.java
@@ -1,8 +1,30 @@
 package k23op1.op1_backend.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import k23op1.op1_backend.domain.Manufacturer;
+import k23op1.op1_backend.domain.ManufacturerRepository;
 
 @Controller
 public class ManufacturerController {
+
+    @Autowired
+    private ManufacturerRepository manufacturerRepository;
+
+    @GetMapping("/addmanufacturer")
+    public String addManufacturers(Model model) {
+        model.addAttribute("manufacturers", new Manufacturer());
+        return "addmanufacturer";
+    }
     
+    @PostMapping("/savemanufacturer")
+    public String saveManufacturer(Manufacturer manufacturer) {
+        manufacturerRepository.save(manufacturer);
+        return "redirect:";
+    }
+
 }
