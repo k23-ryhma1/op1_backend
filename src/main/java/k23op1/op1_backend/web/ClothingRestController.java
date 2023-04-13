d85cb1eaa791390974e259bc4875a0f4ced9ec97
elinarou <elina_rouvinen@outlook.com>
2023-04-13 11:22:55 +0300

    Lisätty Restiin post, put, delete

--- src/main/java/k23op1/op1_backend/web/ClothingRestController.java
+++ src/main/java/k23op1/op1_backend/web/ClothingRestController.java
@@ -3,8 +3,12 @@ package k23op1.op1_backend.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import k23op1.op1_backend.domain.Clothing;
@@ -26,6 +30,26 @@ public class ClothingRestController {
     @GetMapping("/api/clothings/{id}")
     Optional<Clothing> getClothing(@PathVariable Long id) {
         return clothingRepository.findById(id);
+    }
+
+    // Lisää yhden vaatteen
+    @PostMapping("clothings")
+    Clothing newClothing(@RequestBody Clothing newClothing) {
+        return clothingRepository.save(newClothing);
+    }
+
+    // Muokkaa vaatetta
+    @PutMapping("/api/clothings/{id}")
+    Clothing editClothing(@RequestBody Clothing editedClothing, @PathVariable Long id) {
+        editedClothing.setId(id);
+        return clothingRepository.save(editedClothing);
+    }
+
+    // Poistaa vaatteen
+    @DeleteMapping("/api/clothings/{id}")
+    public Iterable<Clothing> deleteClothing(@PathVariable Long id) {
+        clothingRepository.deleteById(id);
+        return clothingRepository.findAll();
     }
     
 }
--- src/main/java/k23op1/op1_backend/web/ManufacturerRestController.java
+++ src/main/java/k23op1/op1_backend/web/ManufacturerRestController.java
@@ -3,8 +3,12 @@ package k23op1.op1_backend.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import k23op1.op1_backend.domain.Manufacturer;
@@ -24,8 +28,28 @@ public class ManufacturerRestController {
     
     // Palauttaa yhden valmistajan
     @GetMapping("/api/manufacturers/{id}")
-    Optional<Manufacturer> getClothing(@PathVariable Long id) {
+    Optional<Manufacturer> getManufacturer(@PathVariable Long id) {
         return manufacturerRepository.findById(id);
+    }
+
+    // Lisää yhden valmistajan
+    @PostMapping("manufacturers")
+    Manufacturer newManufacturer(@RequestBody Manufacturer newManufacturer) {
+        return manufacturerRepository.save(newManufacturer);
+    }
+
+    // Muokkaa valmistajaa
+    @PutMapping("/api/manufacturers/{id}")
+    Manufacturer editManufacturer(@RequestBody Manufacturer editedManufacturer, @PathVariable Long id) {
+        editedManufacturer.setId(id);
+        return manufacturerRepository.save(editedManufacturer);
+    }
+
+    // Poistaa valmistajan
+    @DeleteMapping("/api/manufacturers/{id}")
+    public Iterable<Manufacturer> deleteManufacturer(@PathVariable Long id) {
+        manufacturerRepository.deleteById(id);
+        return manufacturerRepository.findAll();
     }
     
 }
