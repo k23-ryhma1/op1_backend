0e9b8128941d9a47d267e69ace2a5b9b57852618
elinarou <elina_rouvinen@outlook.com>
2023-04-17 19:17:23 +0300

    Siistitty koodia

--- src/main/java/k23op1/op1_backend/web/ClothingController.java
+++ src/main/java/k23op1/op1_backend/web/ClothingController.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-
 import org.springframework.web.bind.annotation.GetMapping;
 
 import k23op1.op1_backend.domain.Clothing;
