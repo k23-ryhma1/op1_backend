2d35a25a76bf5f0d0e6201ef82f91e49072de1dd
elinarou <elina_rouvinen@outlook.com>
2023-04-20 08:30:42 +0300

    Poistettu turha import

--- src/main/java/k23op1/op1_backend/domain/Clothing.java
+++ src/main/java/k23op1/op1_backend/domain/Clothing.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 
