4087075e486c17a5de19b88fc06989e6a459268b
zokaas <bozic.zorana@gmail.com>
2023-05-10 16:28:21 +0300

    customeri testiin

--- src/main/java/k23op1/op1_backend/Op1BackendApplication.java
+++ src/main/java/k23op1/op1_backend/Op1BackendApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import k23op1.op1_backend.domain.ClothingSize;
+import k23op1.op1_backend.domain.ClothingSizeRepository;
+import k23op1.op1_backend.domain.Customer;
+import k23op1.op1_backend.domain.CustomerRepository;
+import k23op1.op1_backend.domain.Manufacturer;
+import k23op1.op1_backend.domain.ManufacturerRepository;
 import k23op1.op1_backend.domain.Product;
 import k23op1.op1_backend.domain.ProductRepository;
 import k23op1.op1_backend.domain.Type;
 import k23op1.op1_backend.domain.TypeRepository;
-import k23op1.op1_backend.domain.ClothingSize;
-import k23op1.op1_backend.domain.ClothingSizeRepository;
-import k23op1.op1_backend.domain.Manufacturer;
-import k23op1.op1_backend.domain.ManufacturerRepository;
 
 @SpringBootApplication
 public class Op1BackendApplication {
@@ -22,8 +24,9 @@ public class Op1BackendApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(ProductRepository productRepository, ManufacturerRepository manufacturerRepository, 
-		ClothingSizeRepository clothingSizeRepository, TypeRepository typeRepository){
+	public CommandLineRunner demo(ProductRepository productRepository, ManufacturerRepository manufacturerRepository,
+			ClothingSizeRepository clothingSizeRepository, TypeRepository typeRepository,
+			CustomerRepository customerRepository) {
 		return (arg) -> {
 			Manufacturer m1 = new Manufacturer("Purina");
 			Manufacturer m2 = new Manufacturer("Pedigree");
@@ -50,6 +53,13 @@ public class Op1BackendApplication {
 			productRepository.save(p1);
 			productRepository.save(p2);
 			productRepository.save(p3);
+
+			Customer c1 = new Customer("Riku", "Rekka", "0409620451", "riku@email.com");
+			Customer c2 = new Customer("Saku", "Sauna", "0401234551", "saku@email.com");
+			Customer c3 = new Customer("Tiina", "Tikka", "0407620331", "tiina@email.com");
+			customerRepository.save(c1);
+			customerRepository.save(c2);
+			customerRepository.save(c3);
 		};
 	}
 }
