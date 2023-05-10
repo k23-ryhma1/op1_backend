5a2076a92faa079e57580ecbf17704f998954b22
zokaas <bozic.zorana@gmail.com>
2023-05-10 16:28:43 +0300

    customer controller

---
+++ src/main/java/k23op1/op1_backend/web/CustomerController.java
@@ -0,0 +1,23 @@
+package k23op1.op1_backend.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import k23op1.op1_backend.domain.CustomerRepository;
+
+@Controller
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    // Asiakaslistaus
+    @GetMapping("/listcustomers")
+    public String customerList(Model model) {
+        model.addAttribute("customers", customerRepository.findAll());
+        return "listcustomers";
+    }
+
+}
