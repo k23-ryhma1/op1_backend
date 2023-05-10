2ee0b8be6c939adba3e9e88bee29cd282c3010aa
Teemu Asikainen <teemu.asikainen@myy.haaga-helia.fi>
2023-05-10 14:01:48 +0300

    Lisätty MariaDB tietokanta ja tehty konfiguraatiot, jotta se toimii nykyisessä Front Endissä

---
+++ src/main/java/k23op1/op1_backend/SecurityConfig.java
@@ -0,0 +1,19 @@
+package k23op1.op1_backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.disable());
+        return http.build();
+    }
+    
+}
