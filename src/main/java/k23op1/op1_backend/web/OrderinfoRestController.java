20e7610058729291f8649cc17bbfe94b7bfe6a22
elinarou <elina_rouvinen@outlook.com>
2023-05-20 15:46:51 +0300

    Lisätty post, put ja delete OrderinfoRestControlleriin

--- src/main/java/k23op1/op1_backend/web/OrderinfoRestController.java
+++ src/main/java/k23op1/op1_backend/web/OrderinfoRestController.java
@@ -3,8 +3,12 @@ package k23op1.op1_backend.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import k23op1.op1_backend.domain.Orderinfo;
@@ -26,6 +30,26 @@ public class OrderinfoRestController {
     @GetMapping("/api/orderinfoes/id/{id}")
     Optional<Orderinfo> getProduct(@PathVariable Long id) {
         return orderinfoRepository.findById(id);
+    }
+    
+    // Lisää yhden tilauksen
+    @PostMapping("/api/orderinfoes")
+    Orderinfo newOrderinfo(@RequestBody Orderinfo newOrderinfo) {
+        return orderinfoRepository.save(newOrderinfo);
+    }
+
+    // Muokkaa tilausta
+    @PutMapping("/api/orderinfoes/{id}")
+    Orderinfo editOrderinfo(@RequestBody Orderinfo editedOrderinfo, @PathVariable Long id) {
+        editedOrderinfo.setId(id);
+        return orderinfoRepository.save(editedOrderinfo);
+    }
+
+    // Poistaa tilauksen
+    @DeleteMapping("/api/orderinfoes/{id}")
+    public Iterable<Orderinfo> deleteOrderinfo(@PathVariable Long id) {
+        orderinfoRepository.deleteById(id);
+        return orderinfoRepository.findAll();
     }
     
 }
