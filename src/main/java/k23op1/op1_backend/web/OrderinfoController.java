05c4f4d6f5662a2c0f55eacee9f46fc7da302e90
zokaas <bozic.zorana@gmail.com>
2023-05-21 17:32:30 +0300

    orderstatusta lisää / kesken

--- src/main/java/k23op1/op1_backend/web/OrderinfoController.java
+++ src/main/java/k23op1/op1_backend/web/OrderinfoController.java
@@ -3,8 +3,14 @@ package k23op1.op1_backend.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
+import k23op1.op1_backend.domain.Orderinfo;
 import k23op1.op1_backend.domain.OrderinfoRepository;
 
 @Controller
@@ -19,4 +25,21 @@ public class OrderinfoController {
         return "orderinfo";
     }
 
+    // Tuotteen muokkaus
+    @GetMapping("/editorderstatus/{id}")
+    public String editOrderstatus(@PathVariable("id") Long orderinfoId, Model model) {
+        model.addAttribute("orderstatus", orderinfoRepository.findById(orderinfoId));
+        return "editorderstatus";
+    }
+
+    // Tallennus
+    @PostMapping("/saveorderstatus")
+    public String saveorderstatus(@Valid @ModelAttribute("orderinfo") Orderinfo orderinfo,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "editorderstatus";
+        }
+        orderinfoRepository.save(orderinfo);
+        return "redirect:listcustomers";
+    }
 }
